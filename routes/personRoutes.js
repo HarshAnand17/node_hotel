7b081fa908d3cf89df4f3e98e723395bf381c803
Harsh Anand <harahanand17@gmail.com>
2025-07-07 09:40:31 +0530

    upload

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const Person=require('./../models/Person');
+const{jwtAuthMiddleware,genrateToken}=require('./../jwt')
+
 //Post route to add a person
-router.post('/',async (req,res)=>{
+router.post('/signup',async (req,res)=>{
     try{
       const data=req.body //Assuming the request body contains the person data
       //Create a new Person document using the Mongoose model
@@ -10,7 +12,15 @@ router.post('/',async (req,res)=>{
       //Save the new Person to the databse
       const response=await newPerson.save();
       console.log('data saved');
-      res.status(200).json(response);
+   
+      const payload={
+        id:response.id,
+        username:response.username
+      }
+    //  console.log(JSON.stringify(payload));
+      const token=genrateToken(payload);
+      console.log("Token is : ",token)
+      res.status(200).json({response:response,token:token});
       
     }
     catch(err) {
@@ -19,8 +29,24 @@ router.post('/',async (req,res)=>{
     }
 })
 
+//Profile route
+router.get('/profile',jwtAuthMiddleware,async(req,res)=>{
+    try{
+      //req.user is data attached by jwtAuthMiddleware
+      //it contains the user data extracted from the JWT token
+      const userData=req.user;//Extract user data from the request object (payload of the jwt token)
+      console.log("User Data :",userData);
+      const userId=userData.id;
+      const user=await Person.findById(userId);
+      res.status(200).json({user})
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({error:'Interna Server Error'});
+    }
+})
 //get method to get the person
- router.get('/',async (req,res)=>{
+ router.get('/',jwtAuthMiddleware,async (req,res)=>{
     try{
         const data=await Person.find();
         console.log('data fetched');
@@ -31,7 +57,32 @@ router.post('/',async (req,res)=>{
       res.status(500).json({error:'Internal Server Error'});
     }
  })
+ //Login Route
+  router.post('/login',async(req,res)=>{
+    try{
+        //Extract username and password from the request body
+        const{username,password}=req.body;
 
+        //find the user by username
+        const user=await Person.findOne({username:username});
+        //if user does not exist or password does not match,return error
+        if(!user || !(await user.comparePassword(password))) {
+          return res.status(401).json({error:'Invalid username or password'})
+        }
+        //generate token
+        const payload={
+          id:user.id,
+          username:user.username
+        }
+        const token=genrateToken(payload);
+        //return token as response
+        res.json({token})
+    }
+    catch(err) {
+      console.log(err);
+      res.status(500).json({error:'Internal Server Error'});
+    }
+  })
   router.get('/:workType',async(req,res)=>{
       try{
          const workType=req.params.workType;//Extract the work type from the URL parameter
